ae7b41e0402778e14411067619c6bcb36de65a3f
fernancumez <fernancumez@gmail.com>
2020-08-20 16:39:56 -0600

    Creacion de mi primer componente

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div style="background: red">
+      <MiComponente />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import MiComponente from "./components/MiComponente.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    MiComponente,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="micomponente">
+    <hr />
+    <h1>Este es mi componente</h1>
+    <h2>{{ subtitulo }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MiComponente",
+  data() {
+    return {
+      subtitulo: "Soy el subtitulo del componente",
+    };
+  },
+};
+</script>
